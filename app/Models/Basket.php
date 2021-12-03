6d3dc4989c0b78d5c3231730b37d2f441a680fd5
Firdavs <firdavs3253@gmail.com>
2021-12-03 13:03:33 +0500

    fixed models

--- app/Models/Basket.php
+++ app/Models/Basket.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Basket extends Model
 {
     protected $guarded = ['id'];
+
+    /**
+     * @return string
+     */
+    public function phone(): string
+    {
+        preg_match("/(998)(\d{2})(\d{3})(\d{2})(\d{2})/", $this->phone, $matches);
+        return "+{$matches[1]} {$matches[2]} {$matches[3]} {$matches[4]} {$matches[5]}";
+    }
 }
--- app/Models/BotUser.php
+++ app/Models/BotUser.php
@@ -16,4 +16,13 @@ class BotUser extends Model
     public $incrementing = false;
 
     protected $fillable = ['chat_id', 'name', 'phone', 'username', 'is_finished', 'language'];
+
+    /**
+     * @return string
+     */
+    public function phone(): string
+    {
+        preg_match("/(998)(\d{2})(\d{3})(\d{2})(\d{2})/", $this->phone, $matches);
+        return "+{$matches[1]} {$matches[2]} {$matches[3]} {$matches[4]} {$matches[5]}";
+    }
 }
