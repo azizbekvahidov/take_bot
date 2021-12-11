512d2283b2998352cf672164ccd01e7ccd3c6bd9
Firdavs <firdavs3253@gmail.com>
2021-12-11 17:31:03 +0500

    edited api.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('telegram-bot-connect', 'Api\\TelegramController');
+Route::post('telegram-bot-connect', 'Api/TelegramController');
