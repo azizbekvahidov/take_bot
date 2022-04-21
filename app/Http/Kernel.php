a15e54ea394f2829fffdfedb9d657851b83591f5
Firdavs Ibodullaev <firdavs3253@gmail.com>
2022-04-21 12:21:11 +0500

    test for session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,12 +42,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
